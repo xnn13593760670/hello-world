2ad3feeefa552907362babb913de48180a42855f
xnn135937606700 <15393719325@163.com>
2021-01-15 23:03:37 +0800

    update .gitignore

---
+++ ConcurrentProgramming/src/com/xnn/s44/CompletableFutureDemo.java
@@ -0,0 +1,58 @@
+package com.xnn.s44;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class CompletableFutureDemo {
+
+    private final CompletableFuture<Void> completableFuture = new CompletableFuture<Void>();
+
+    public static void main(String[] args)
+            throws Exception {
+        CompletableFutureDemo completableFutureDemo = new CompletableFutureDemo();
+        System.out.println(completableFutureDemo.getPrices());
+    }
+
+    private Set<Integer> getPrices() {
+        Set<Integer> prices = Collections.synchronizedSet(new HashSet<Integer>());
+        CompletableFuture<Void> task1 = CompletableFuture.runAsync(new Task(123, prices));
+        CompletableFuture<Void> task2 = CompletableFuture.runAsync(new Task(456, prices));
+        CompletableFuture<Void> task3 = CompletableFuture.runAsync(new Task(789, prices));
+
+        CompletableFuture<Void> allTasks = CompletableFuture.allOf(task1, task2, task3);
+        try {
+            allTasks.get(3, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+        } catch (ExecutionException e) {
+        } catch (TimeoutException e) {
+        }
+        return prices;
+    }
+    private class Task implements Runnable {
+
+        Integer productId;
+        Set<Integer> prices;
+
+        public Task(Integer productId, Set<Integer> prices) {
+            this.productId = productId;
+            this.prices = prices;
+        }
+
+        @Override
+        public void run() {
+            int price = 0;
+            try {
+                Thread.sleep((long) (Math.random() * 4000));
+                price = (int) (Math.random() * 4000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            prices.add(price);
+        }
+    }
+}
